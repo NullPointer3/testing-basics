6ae373f72dc43028e17fc3799830108110001e9f
Noah <nuhumusah874@gmail.com>
2024-12-24 20:35:46 +0000

    basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState<string[]>([]);
+  const [item, setItem] = useState('');
+
+  const onItemChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    setItem(prev => (
+      prev = evt.target.value
+    ));
+  }
+
+  const addItem = (evt: React.FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    setItems(prev => [...prev, item]);
+    setItem('');
+  }
+
+  const submitDisabled = !item;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      id='App'
+    >
+      <table>
+        <thead>
+          <tr>
+            <th>Items</th>
+          </tr>
+        </thead>
+        <tbody>
+          { items.map((item, idx) => (
+            <tr
+              key={idx}
+            >
+              <td>{item}</td>
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <th>
+              <form
+                onSubmit={addItem}
+              >
+                <div>
+                  <input 
+                    type="text" 
+                    placeholder='Add item...' 
+                    value={item}
+                    onChange={onItemChange} 
+                  />
+                </div>
+                <button
+                  disabled={submitDisabled}
+                >
+                  Add item
+                </button>
+              </form>
+            </th>
+          </tr>
+        </tfoot>
+      </table>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
