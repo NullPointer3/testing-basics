582e1861c4825ad691bb7a55542b73bc947315e7
Noah <nuhumusah874@gmail.com>
2025-01-01 19:18:06 +0000

    test refactor

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -12,45 +12,19 @@ const setUp = () => {
   return { headerElement, buttonElement, inputElement, disabledButtonElement };
 }
 
-describe('App', () => {
-
+describe('App',()=> {
   it('should have the `th` "Items"', () => {
     const { headerElement } = setUp();
     expect(headerElement).toBeInTheDocument();
   })
 
-  it('should have a `button` element', () => {
+  it('Should have a `button` element', () => {
     const { buttonElement } = setUp();
     expect(buttonElement).toBeInTheDocument();
   })
 
-  it('should have an `input` element', () => {
+  it('Should have an `input` element', () => {
     const { inputElement } = setUp();
     expect(inputElement).toBeInTheDocument();
-  })
-
-  it('renders a disabled `button` with text "Add item"', () => {
-    const { disabledButtonElement } = setUp();
-    expect(disabledButtonElement).toBeDisabled();
-  })
-
-  it('the input should innitially be empty', () => {
-    const { inputElement } = setUp();
-    expect(inputElement).toHaveValue('');
-  })
-
-  it('allows user to text in the input and submit', () => {
-    const { inputElement, buttonElement } = setUp();
-    // Simulate typing into the input
-    fireEvent.change(inputElement, { target: { value: 'Test Input' } });
-    expect(inputElement).toHaveValue('Test Input');
-    expect(buttonElement).not.toBeDisabled();
-
-     // Simulate button click
-    fireEvent.click(buttonElement);
-
-    // Assert submitted value is displayed
-    const submittedElement = screen.getByText('Test Input');
-    expect(submittedElement).toBeInTheDocument();
   })
 })
